e883890c291a752074d9b637fc2a4fe8c220e2d7
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-11-04 00:00:46 +0100

    Expanded docs. Improved verbose output.

--- R/downloadAndPrepareGEOData.R
+++ R/downloadAndPrepareGEOData.R
@@ -1,47 +1,57 @@
 #' Download GEO dataset
 #'
-#' Function for downloading a GEO dataset. Simply a wrapper for \code{getGEO}
-#' and \code{getGEOSuppFiles}.
+#' Function for downloading a GEO dataset. Simply a wrapper for
+#' and \code{getGEOSuppFiles} which downloads and unzips the
+#' \code{.CEL} files in the GEO series.
 #'
 #' @param geo_nbr The GEO ascession number.
 #' @param destdir The destination dir of the downloaded files.
 #' @param verbose Signal the process.
 #' @param clean Should the strictly unnessesary files be deleted?
+#' @note The function will overwrite existing files in the \code{destdir}.
+#' @author Anders Ellern Bilgrau,
+#'   Steffen Falgreen Larsen
+#' @examples
+#' \dontrun{
+#' downloadAndPrepareGEOData(geo_nbr = "GSE18376",
+#'                           destdir = tempdir())
+#' }
 #' @export
 downloadAndPrepareGEOData <- function(geo_nbr,
                                       destdir = getwd(),
                                       verbose = TRUE,
                                       clean = TRUE) {
   stopifnot(require("GEOquery"))
+  if (verbose) cat("Preparing GEO", geo_nbr, "data\n")
 
   # Download data
-  if (verbose) cat("Downloading .CEL files.\n")
+  if (verbose) cat("Downloading .CEL files...\n")
   dl.dir <- file.path(destdir, geo_nbr)
+  dir.create(dl.dir)
   dl <- getGEOSuppFiles(GEO = geo_nbr, makeDirectory = FALSE, baseDir = dl.dir)
 
   # Untar the file bundle
-  if (verbose) cat("Untaring the .CEL files.\n")
+  if (verbose) cat("Untaring the RAW file...\n")
   tar.file <- list.files(path = dl.dir, pattern = "RAW.tar$", full.names = TRUE)
   exdir <- dl.dir
   untar(tarfile = tar.file, exdir = exdir)
 
   # Unzip the files
-  if (verbose) cat("Unzipping the .CEL files.\n")
+  if (verbose) cat("Unzipping the .CEL files...\n")
   gz.files <- list.files(exdir, pattern = "\\.gz$", ignore.case = TRUE,
                          full.names = TRUE)
   for (file in gz.files) {
-    gunzip(file, remove = TRUE)
+    gunzip(file, overwrite = TRUE, remove = TRUE)
   }
   cel.files <- list.files(exdir, pattern = "\\.cel$",
                           ignore.case = TRUE, full.names = TRUE)
 
   # Clean-up if wanted
   if (clean) {
-    if (verbose) cat("Removing unnessary files.\n")
-    file.remove(tar.file)
+    if (verbose) cat("Removing unnessary files...\n")
     file.remove(rownames(dl))
   }
 
+  if (verbose) cat("Done.")
   return(invisible(data.frame(cel.files, gz.files)))
 }
-
