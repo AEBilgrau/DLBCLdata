b64c493288e911fb52e6791b6e145d6a0e8d7637
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-11-06 15:38:01 +0100

    Added functionality to preprocess microarrays and install and downloaded brainarray CDFs.

---
+++ R/preprocessCELFiles.R
@@ -0,0 +1,42 @@
+
+#' @importFrom affy read.affybatch
+#' @importFrom affyio read.celfile.header
+#' @export
+preprocessCELFiles <- function(cel_files = list.files(path, pattern = "\\CEL$",
+                                                  full.names = TRUE),
+                               cdf = "affy",
+                               target = c("core", "full", "feature"),
+                               version = getLatestVersion(),
+                               background = TRUE,
+                               normalize = TRUE,
+                               path = getwd(),
+                               ...) {
+  stopifnot(require("affyio"))
+  cel_files <- normalizePath(cel_files)
+  array_type <- read.celfile.header(cel_files[1])$cdfName
+
+  if (tolower(cdf) == "affy") {
+    stopifnot(require("oligo"))
+    target <- match.arg(target)
+
+    # Load expression set
+    es <- oligo::read.celfiles(cel_files)
+
+    # RMA normalize
+    if (class(es) %in% c("ExonFeatureSet","HTAFeatureSet","GeneFeatureSet")) {
+      es_rma <- oligo::rma(es, background = background,
+                           normalize = normalize, target = target)
+    } else {
+      es_rma <- oligo::rma(es, background = background,
+                           normalize = normalize)
+    }
+
+  } else {
+    stopifnot(require("affy"))
+    req <- requireBrainarray(array_type = array_type, custom_cdf = cdf,
+                             version = version)
+    es_rma <- justRMA(filenames = cel_files, verbose = TRUE,
+                      cdfname = getCustomCDFName(req$brain_dat, array_type))
+  }
+  return(es_rma)
+}
---
+++ R/requireBrainarray.R
@@ -0,0 +1,51 @@
+#' Installs brainarray CDF and probe information packages
+#'
+#' This package downloads, installs, and/or loads the brainarray CDF and probe
+#' enviroments from the Brainarray website.
+#'
+#' @param array_type A character of length 1 giving the giving the name of the'
+#'   array. (Preferably as returned by \code{affyio::read.celfile.header})
+#' @param custom_cdf A charcter giving the annotation type.
+#' @param version A character of length 1 giving the version to download.
+#' @return Invisibly returns a list of the locations of the saved files.
+#' @author
+#'   Steffen Falgreen
+#'   Anders Ellern Bilgrau <abilgrau (at) math.aau.dk
+#' @seealso \code{\link{tempdir}}, \code{\link{install.packages}}
+#' @references
+#'   See \url{http://brainarray.mbni.med.umich.edu}.
+#' @examples
+#' tmp.path <- requireBrainarray("hgu133a", custom_cdf = "enst")
+#' print(tmp.path)
+#' @export
+requireBrainarray <- function(array_type,
+                              custom_cdf = "enst",
+                              version = getLatestVersion()) {
+  output <- list()
+  brain_dat <- output[["brain_dat"]] <-
+    readBrainarrayTable(custom_cdf = custom_cdf, version = version)
+  base_url <- attributes(brain_dat)$base_url
+  pkgs <- getBrainarrayPackages(brain_dat, array_type = array_type)
+
+  for (pkg in pkgs) {
+    loaded <- suppressWarnings(require(pkg, character.only = TRUE))
+    if (!loaded || packageVersion(pkg) != version) {
+      if (loaded) {
+         message("Installed package version is not ", version,
+                 ". Overwriting installed package.", sep = "")
+      }
+      file.pkg <- paste0(pkg, "_", version, ".tar.gz")
+
+      # Construct url and download package
+      pkg_url <- file.path(paste0(base_url, ".download"), file.pkg)
+      pkg_dest <- output[[pkg]] <- file.path(tempdir(), file.pkg)
+      download.file(url = pkg_url, destfile = pkg_dest)
+
+      # Install and load package
+      install.packages(pkgs = pkg_dest, repos = NULL, type = "source")
+      require(pkg, character.only = TRUE)
+    }
+  }
+
+  return(invisible(output))
+}
