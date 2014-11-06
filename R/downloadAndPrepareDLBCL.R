674ed4a6effd8c0e1b96f5c42db6e4697bd856b8
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-11-06 16:37:35 +0100

    Function to be written.

---
+++ R/downloadAndPrepareDLBCL.R
@@ -0,0 +1,15 @@
+#' Download all curated DLBCL datasets
+#'
+#' Automatically download all curated DLBCL datasets.
+#'
+#' @author
+#'   Anders Ellern Bilgrau <abilgrau (at) math.aau.dk>
+#'   Steffen Falgreen Larsen <sfl (at) rn.dk>
+#' @examples
+#' \dontrun{
+#' res <- downloadAndProcessDLBCL()
+#' }
+#' @export
+downloadAndProcessDLBCL <- function(...) {
+  message("not implemented yet!")
+}
