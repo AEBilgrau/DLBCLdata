1ce64fc45abfd481e67485cff936d28ab60776ea
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-11-07 16:36:18 +0100

    Hacky implementation of getLatestVersion and getBrainarrayTargets

--- R/auxiliary_brainarray.R
+++ R/auxiliary_brainarray.R
@@ -27,6 +27,24 @@ getBrainarrayPackages <- function(brain_dat, array_type) {
   return(r_packages)
 }
 
+version <- "18.0.0"
 
+#' @importFrom XML readHTMLTable
+getBrainarrayTargets <- function(version) {
+  base_url <-
+    paste0("http://brainarray.mbni.med.umich.edu/Brainarray/",
+           "Database/CustomCDF/CDF_download.asp")
+
+  brain_dat <- readHTMLTable(base_url, stringsAsFactors = FALSE)
+  brain_dat <- brain_dat[sapply(brain_dat, is.list)][-(1:2)]
+
+  if (missing(version)) {
+    ans <- as.character(brain_dat[[length(brain_dat)]][["Custom CDF"]])
+  } else {
+    v <- as.numeric(gsub("(^[0-9]+)\\..+$", "\\1", version))
+    ans <- as.character(brain_dat[[v]][["Custom CDF"]])
+  }
+  return(ans)
+}
 
 
--- R/getLatestVersion.R
+++ R/getLatestVersion.R
@@ -1,4 +1,20 @@
 # For future versions of DLBCLdata
+# Currently a very hacky implementation
+# @examples
+# getLatestVersion()
 getLatestVersion <- function() {
-  return("18.0.0")
+  base_url <-
+    paste0("http://brainarray.mbni.med.umich.edu/Brainarray/",
+           "Database/CustomCDF/CDF_download.asp")
+
+  brain_dat <- readHTMLTable(base_url, stringsAsFactors = FALSE)
+  brain_dat <- brain_dat[sapply(brain_dat, is.list)][-(1:2)]
+
+  vX.0.0 <- length(brain_dat)
+  v0.X.0 <- sum(grepl("Click", brain_dat[[vX.0.0]][1,])) - 1
+  v0.0.X <- 0
+
+  return(paste(vX.0.0, v0.X.0, v0.0.X, sep = "."))
 }
+
+
