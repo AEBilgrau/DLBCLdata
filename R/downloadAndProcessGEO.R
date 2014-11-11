a5df7c5e8d576714a9f7c154b8c6b286b1fa3c0c
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-11-11 17:24:47 +0100

    Added ability to normalized in batches.

--- R/downloadAndProcessGEO.R
+++ R/downloadAndProcessGEO.R
@@ -9,23 +9,25 @@
 #' @param ... Arguments passed to \code{\link{preprocessCELFiles}}
 #' @param clean Should the strictly unnessesary files be deleted?
 #' @param verbose Signal the process.
-#' @return Does not return anything. Creates a folder with the downloaded and
-#'   processed files.
+#' @return
+#'   Creates a folder with the downloaded and processed files.
+#'   The processed files are saved as an \code{.Rds} object named
+#'   after the used parameters.
+#'   Invisibly returns the saved object.
 #' @note The function will overwrite existing files in the \code{destdir}.
 #' @author
 #'   Anders Ellern Bilgrau,
 #'   Steffen Falgreen Larsen
 #' @examples
 #' \dontrun{
-#' res <- downloadAndProcessGEO("GSE18376",
+#' res <- downloadAndProcessGEO(geo_nbr = "GSE18376",
 #'                              cdf = "brainarray",
 #'                              target = "ENSG",
 #'                              clean = FALSE)
-#' res2 <- downloadAndProcessGEO("GSE10846",
-#'                              cdf = "brainarray",
-#'                              target = "ENSG",
-#'                              clean = FALSE)
-#'  ress <- list()
+#' res2 <- downloadAndProcessGEO(geo_nbr = "GSE10846",
+#'                               cdf = "brainarray",
+#'                               target = "ENSG",
+#'                               clean = FALSE)
 #' for (gse in DLBCL_overview$GSE) {
 #'    ress[[gse]] <- downloadAndProcessGEO(gse,
 #'                              cdf = "brainarray",
@@ -54,8 +56,33 @@ downloadAndProcessGEO <- function(geo_nbr,
   cel_files <- downloadAndPrepareCELFiles(geo_nbr = geo_nbr, destdir = destdir,
                                           clean = clean, verbose = verbose)
 
-  # Preprocess array data by RMA
-  es <- preprocessCELFiles(cel_files, ...)
+  # Add filenames to cleaned data
+  clean_meta_data$file <-
+    cel_files[match(clean_meta_data$GSM, basenameSansCEL(cel_files))]
+  stopifnot(all(basenameSansCEL(clean_meta_data$file) == clean_meta_data$GSM))
+
+  # Checks and warnings
+  if (nrow(clean_meta_data) != length(cel_files)) {
+    warning("The number of subjects in the metadata (", nrow(clean_meta_data),
+            ") does not equal the number of cel files (",
+            length(cel_files), "). Only using the files in the metadata.")
+  }
+  if (!all(basenameSansCEL(cel_files) %in%
+             basenameSansCEL(clean_meta_data$CEL))) {
+    warning("For ", geo_nbr, " not all downloaded CEL files are in the metadata")
+  }
+
+  # Preprocess array data by RMA for each batch
+  if (is.null(clean_meta_data$Batch) && is.null(clean_meta_data$CEL)) {
+    es <- preprocessCELFiles(cel_files, ...)
+  } else {
+    if (verbose) {
+      message("Batches detected. RMA normalizing each of the batches: ",
+              paste0(levels(clean_meta_data$Batch), collapse = " "))
+    }
+    batch_list <- with(clean_meta_data, split(file, Batch))
+    es <- lapply(batch_list, preprocessCELFiles, ...)
+  }
 
   # Save Rds file
   a <- attributes(es)
