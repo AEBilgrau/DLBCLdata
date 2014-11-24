e9e2e77684a8cab912891fd2899a05ee0fa46f8e
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-11-24 23:45:59 +0100

    Fixed bug with newest version 19.0.0.

--- R/getLatestVersion.R
+++ R/getLatestVersion.R
@@ -1,7 +1,14 @@
-# For future versions of DLBCLdata
-# Currently a very hacky implementation
-# @examples
-# getLatestVersion()
+#' Get latest brainarray version number
+#'
+#' Currently a very hacky implementation. To be updated later. The function
+#' currently reads the tables available at
+#' \url{http://brainarray.mbni.med.umich.edu/Brainarray/Database/CustomCDF/CDF_download.asp}
+#' to determine the latest version
+#'
+#' @return Returns a string on the form X.Y.Z giving the latest version.
+#' @examples
+#' getLatestVersion()
+#' @export
 getLatestVersion <- function() {
   base_url <-
     paste0("http://brainarray.mbni.med.umich.edu/Brainarray/",
@@ -11,7 +18,8 @@ getLatestVersion <- function() {
   brain_dat <- brain_dat[sapply(brain_dat, is.list)][-(1:2)]
 
   vX.0.0 <- length(brain_dat)
-  v0.X.0 <- sum(grepl("Click", brain_dat[[vX.0.0]][1,])) - 1
+  tmp <- brain_dat[[vX.0.0]]
+  v0.X.0 <- sum(grepl("Click", tmp[nrow(tmp),])) - 1
   v0.0.X <- 0
 
   return(paste(vX.0.0, v0.X.0, v0.0.X, sep = "."))
