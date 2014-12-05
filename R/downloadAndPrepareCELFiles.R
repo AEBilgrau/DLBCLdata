6f8b0cc4ff473511e35d1690285eae144f643478
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-12-05 14:17:00 +0100

    cat to message

--- R/downloadAndPrepareCELFiles.R
+++ R/downloadAndPrepareCELFiles.R
@@ -25,17 +25,17 @@ downloadAndPrepareCELFiles <- function(geo_nbr,
                                        destdir = getwd(),
                                        clean = FALSE,
                                        verbose = TRUE) {
-  if (verbose) cat("Preparing GEO", geo_nbr, "data\n")
+  if (verbose) message("Preparing GEO", geo_nbr, "data")
   dl.dir <- file.path(destdir, geo_nbr)
 
   # Download data if nessesary
   raw_file <- paste0(file.path(dl.dir, geo_nbr), "_RAW.tar")
   if (!file.exists(raw_file)) {
-    if (verbose) cat("Downloading .CEL files...\n")
+    if (verbose) message("Downloading .CEL files...\n")
     dir.create(dl.dir, showWarnings = FALSE)
     getGEOSuppFiles(GEO = geo_nbr, makeDirectory = FALSE, baseDir = dl.dir)
   } else {
-    if (verbose) cat("Compressed .CEL files already downloaded...\n")
+    if (verbose) message("Compressed .CEL files already downloaded...")
   }
 
   # List celfiles (if any)
@@ -45,12 +45,12 @@ downloadAndPrepareCELFiles <- function(geo_nbr,
   if (identical(cel_files, character(0))) {
 
     # Untar the file bundle
-    if (verbose) cat("Untaring the RAW file...\n")
+    if (verbose) message("Untaring the RAW file...\n")
     tar.file <- list.files(path = dl.dir, pattern = "RAW.tar$", full.names = TRUE)
     untar(tarfile = tar.file, exdir = dl.dir)
 
     # Unzip the files
-    if (verbose) cat("Unzipping the .CEL files...\n")
+    if (verbose) message("Unzipping the .CEL files...\n")
     gz_files <- list.files(dl.dir, pattern = "\\.cel.gz$", ignore.case = TRUE,
                            full.names = TRUE)
     for (file in gz_files) {
@@ -61,16 +61,16 @@ downloadAndPrepareCELFiles <- function(geo_nbr,
     cel_files <- list.files(dl.dir, pattern = "\\.cel$",
                             ignore.case = TRUE, full.names = TRUE)
   } else {
-    cat(".CEL files already unpacked...\n")
+    message("(Some) .CEL files already unpacked...\n")
   }
 
   # Clean-up if wanted
   if (clean) {
-    if (verbose) cat("Removing all non CEL files...\n")
+    if (verbose) message("Removing all non CEL files...\n")
     file.remove(setdiff(list.files(dl.dir, full.names = TRUE), cel_files))
   }
 
   cel_files <- normalizePath(cel_files)
-  if (verbose) cat("Download and preparation of CEL files done.\n")
+  if (verbose) message("Download and preparation of CEL files done.\n")
   return(invisible(cel_files))
 }
