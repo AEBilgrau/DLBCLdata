88c7823f150abf9e249b9753899ca1805af873c2
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2015-03-16 13:52:26 +0100

    Using _ in "variable_names". Not "variable.names".

--- R/downloadAndPrepareCELFiles.R
+++ R/downloadAndPrepareCELFiles.R
@@ -25,40 +25,40 @@ downloadAndPrepareCELFiles <- function(geo_nbr,
                                        destdir = getwd(),
                                        clean = FALSE,
                                        verbose = TRUE) {
-  if (verbose) message("Preparing GEO", geo_nbr, "data")
-  dl.dir <- file.path(destdir, geo_nbr)
+  if (verbose) message("Preparing GEO ", geo_nbr, " data")
+  dl_dir <- file.path(destdir, geo_nbr)
 
   # Download data if nessesary
-  raw_file <- paste0(file.path(dl.dir, geo_nbr), "_RAW.tar")
+  raw_file <- paste0(file.path(dl_dir, geo_nbr), "_RAW.tar")
   if (!file.exists(raw_file)) {
     if (verbose) message("Downloading .CEL files...\n")
-    dir.create(dl.dir, showWarnings = FALSE)
-    getGEOSuppFiles(GEO = geo_nbr, makeDirectory = FALSE, baseDir = dl.dir)
+    dir.create(dl_dir, showWarnings = FALSE)
+    getGEOSuppFiles(GEO = geo_nbr, makeDirectory = FALSE, baseDir = dl_dir)
   } else {
     if (verbose) message("Compressed .CEL files already downloaded...")
   }
 
   # List celfiles (if any)
-  cel_files <- list.files(dl.dir, pattern = "\\.cel$",
+  cel_files <- list.files(dl_dir, pattern = "\\.cel$",
                           ignore.case = TRUE, full.names = TRUE)
 
   if (identical(cel_files, character(0))) {
 
     # Untar the file bundle
     if (verbose) message("Untaring the RAW file...\n")
-    tar.file <- list.files(path = dl.dir, pattern = "RAW.tar$", full.names = TRUE)
-    untar(tarfile = tar.file, exdir = dl.dir)
+    tar_file <- list.files(path = dl_dir, pattern = "RAW.tar$", full.names = TRUE)
+    untar(tarfile = tar_file, exdir = dl_dir)
 
     # Unzip the files
     if (verbose) message("Unzipping the .CEL files...\n")
-    gz_files <- list.files(dl.dir, pattern = "\\.cel.gz$", ignore.case = TRUE,
+    gz_files <- list.files(dl_dir, pattern = "\\.cel.gz$", ignore.case = TRUE,
                            full.names = TRUE)
     for (file in gz_files) {
       gunzip(file, overwrite = TRUE, remove = TRUE)
     }
 
     # List celfiles
-    cel_files <- list.files(dl.dir, pattern = "\\.cel$",
+    cel_files <- list.files(dl_dir, pattern = "\\.cel$",
                             ignore.case = TRUE, full.names = TRUE)
   } else {
     message("(Some) .CEL files already unpacked...\n")
@@ -67,7 +67,7 @@ downloadAndPrepareCELFiles <- function(geo_nbr,
   # Clean-up if wanted
   if (clean) {
     if (verbose) message("Removing all non CEL files...\n")
-    file.remove(setdiff(list.files(dl.dir, full.names = TRUE), cel_files))
+    file.remove(setdiff(list.files(dl_dir, full.names = TRUE), cel_files))
   }
 
   cel_files <- normalizePath(cel_files)
