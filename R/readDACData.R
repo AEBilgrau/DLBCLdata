ada62bc69cec2afb4a1386126cec9fdebda4a89e
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2015-03-16 16:20:29 +0100

    Expanded docs. Slightly safer function.

--- R/readDACData.R
+++ R/readDACData.R
@@ -1,18 +1,20 @@
 #' Read DLBCL class files
 #'
 #' Functions to read the internal data files of classified CEL files using
-#' the DLBCL Automatic Classifier (DAC).
+#' the DLBCL Automatic Classifier (DAC). These were manually supplied to the
+#' DAC Windows interface.
 #'
-#' @param geo_nbr A character string giving the GSE number.
+#' @param geo_nbr A \code{character} string giving the GSE number.
 #' @return A \code{data.frame} of the predicted classes and the class
 #'   probabilities.
 #' @examples
-#' readDACData("GSE19246")
+#' head(readDACData("GSE19246"))
 #' @keywords internal
 #' @export
 readDACData <- function(geo_nbr) {
   extdir <- system.file("extdata", package = "DLBCLdata")
-  files <- list.files(extdir, pattern = "\\.txt$", full.names = TRUE)
+  files <- list.files(extdir, pattern = "^predictions-.+\\.txt$",
+                      full.names = TRUE)
   file <- grep(geo_nbr, files, value = TRUE)
   dat <- lapply(file, read.table, header = TRUE)
   names(dat) <- gsub("predictions-|\\.txt$", "", basename(file))
