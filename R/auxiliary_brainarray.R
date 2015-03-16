8273f9465165b3514a8d5289a271a12814954bde
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2015-03-16 16:15:56 +0100

    Fixed bug. Should use the first number.

--- R/auxiliary_brainarray.R
+++ R/auxiliary_brainarray.R
@@ -38,12 +38,13 @@ getBrainarrayTargets <- function(version) {
                      "Database/CustomCDF/CDF_download.asp")
 
   brain_dat <- readHTMLTable(base_url, stringsAsFactors = FALSE)
-  brain_dat <- brain_dat[sapply(brain_dat, is.list)][-(1:2)]
+  brain_dat <- brain_dat[sapply(brain_dat, is.data.frame)]
+  brain_dat <- brain_dat[-(1:2)]
 
   if (missing(version)) {
     ans <- as.character(brain_dat[[length(brain_dat)]][["Custom CDF"]])
   } else {
-    v <- getSubversion(version, n = 2)
+    v <- getSubversion(version, n = 1)
     ans <- as.character(brain_dat[[v]][["Custom CDF"]])
   }
   return(ans)
