5978dbdfe27513257b58dcb616175fa989d531ff
AEBilgrau <anders.ellern.bilgrau@gmail.com>
2015-03-19 07:55:29 +0100

    Added a test for downloading and processing everything.

---
+++ inst/tests/test-downloadAndProcessGEO.R
@@ -0,0 +1,55 @@
+context("Download and process all DLBCL datasets")
+
+
+data(DLBCL_overview)
+
+if (deep_test) {  # deep_test is defined in ./test-all.R
+
+  for (gse in DLBCL_overview$GSE) {
+
+    cat("\n\n\n\n\n\n\n\n #### ", gse, " #### \n\n\n\n")
+
+    res <- downloadAndProcessGEO(geo_nbr = gse,
+                                 cdf = "brainarray",
+                                 target = "ensg",
+                                 clean = FALSE)
+
+    cat("\n", gse,  "downloaded and preprocessed successfully!\n")
+
+    test_that("downloadAndProcessGEO works for all DLBCL sets", {
+      expect_that(res, is_a("list"))
+      expect_that(names(res), equals(c("es", "metadata", "call")))
+      expect_that(res$es, is_a("list"))
+      expect_that(res$metadata, is_a(gse))
+      expect_that(res$metadata, is_a("data.frame"))
+      expect_that(res$call, is_a("call"))
+
+      # All expression values should not be na
+      expect_that(all(sapply(res$es, inherits, "ExpressionSet")), is_true())
+      expect_that(any(sapply(res$es, function(x) anyNA(exprs(x)))), is_false())
+    })
+  }
+}
+
+# GSE10846 OK
+# GSE12195 OK
+# GSE19246 OK
+# GSE22895 OK
+# GSE31312 OK
+# GSE4475  OK
+# GSE22470 OK (FIXED, PROBLEMS WITH FILENAMES vs GEONUMBERS)
+# GSE34171 OK (FIXED ???) PROBLEMS WITH DOWNLOAD
+# GSE11318 OK (FIXED ???)
+
+#
+# Warning messages:
+#   1: In readLines(fname) :
+#   incomplete final line found on 'C:/Users/Anders/Documents/GitHub/DLBCLdata/GSE19246/GSE19246_series_matrix.txt.gz'
+# 2: In cleanMetadata.GSE10846(meta_data) : NAs introduced by coercion
+# 3: In cleanMetadata.GSE10846(meta_data) : NAs introduced by coercion
+# 4: In cleanMetadata.GSE10846(meta_data) : NAs introduced by coercion
+# 5: In cleanMetadata.GSE10846(meta_data) : NAs introduced by coercion
+# 6: In downloadAndProcessGEO(geo_nbr = gse, cdf = "brainarray", target = "ensg",  :
+#                               Not all downloaded CEL files are in the metadata
+#                             7: In downloadAndProcessGEO(geo_nbr = gse, cdf = "brainarray", target = "ensg",  :
+#                                                           Not all downloaded CEL files are in the metadata
--- tests/test-all.R
+++ tests/test-all.R
@@ -1,4 +1,6 @@
 library("testthat")
 library("DLBCLdata")
 
+deep_test <- FALSE  # Do extensive testing --- SLOW!
+
 test_package("DLBCLdata")
