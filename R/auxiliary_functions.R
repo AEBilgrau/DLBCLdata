4ffd300faf3723462eed03b6b4f824c042084f90
AEBilgrau <anders.ellern.bilgrau@gmail.com>
2015-03-19 07:59:11 +0100

    Document, export, and expand basenameSansCEL

--- R/auxiliary_functions.R
+++ R/auxiliary_functions.R
@@ -1,6 +1,17 @@
 
+#' Get file basename and remove .CEL extension
+#'
+#' Functions as \code{\link{basename}} but also remove any \code{.CEL}
+#' or \code{.CEL.gz} extension. The function is not case sensitive of toward
+#' the extension.
+#'
+#' @param x A \code{character} vector of file names.
+#' @return A \code{character} vector of the same length as \code{x} with the
+#'   basenames and no \code{.CEL} extension.
+#' @seealso \code{\link{basename}}
+#' @export
 basenameSansCEL <- function(x) {
-  return(gsub("\\.cel", "", basename(x), ignore.case = TRUE))
+  return(gsub("\\.cel$|\\.cel.gz$", "", basename(x), ignore.case = TRUE))
 }
 
 getAffyTargets <- function() {
