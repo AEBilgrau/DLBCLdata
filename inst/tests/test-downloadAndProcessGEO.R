00b080cdc5e48271c051ae6fe2b949609736baa2
AEBilgrau <anders.ellern.bilgrau@gmail.com>
2015-04-12 00:52:30 +0200

    clean-up

--- inst/tests/test-downloadAndProcessGEO.R
+++ inst/tests/test-downloadAndProcessGEO.R
@@ -5,22 +5,22 @@ data(DLBCL_overview)
 
 if (deep_test) {  # deep_test is defined in ./test-all.R
   exclude <- character(0)
-  for (gse in setdiff(DLBCL_overview$GSE, exclude)) {
+  for (geo_nbr in setdiff(DLBCL_overview$GSE, exclude)) {
 
-    cat("\n\n\n\n\n\n\n\n #### ", gse, " #### \n\n\n\n")
+    cat("\n\n\n\n\n\n\n\n #### ", geo_nbr, " #### \n\n\n\n")
 
-    res <- downloadAndProcessGEO(geo_nbr = gse,
+    res <- downloadAndProcessGEO(geo_nbr = geo_nbr,
                                  cdf = "brainarray",
                                  target = "ensg",
                                  clean = FALSE)
 
-    cat("\n", gse,  "downloaded and preprocessed successfully!\n")
+    cat("\n", geo_nbr,  "downloaded and preprocessed successfully!\n")
 
-    test_that(paste("downloadAndProcessGEO works for", gse), {
+    test_that(paste("downloadAndProcessGEO works for", geo_nbr), {
       expect_that(res, is_a("list"))
       expect_that(names(res), equals(c("es", "metadata", "call")))
       expect_that(res$es, is_a("list"))
-      expect_that(res$metadata, is_a(gse))
+      expect_that(res$metadata, is_a(geo_nbr))
       expect_that(res$metadata, is_a("data.frame"))
       expect_that(res$call, is_a("call"))
 
@@ -31,25 +31,12 @@ if (deep_test) {  # deep_test is defined in ./test-all.R
   }
 }
 
-# GSE10846 OK
-# GSE12195 OK
-# GSE19246 OK
-# GSE22895 OK
-# GSE31312 OK
-# GSE4475  OK
-# GSE22470 OK (FIXED, PROBLEMS WITH FILENAMES vs GEONUMBERS)
-# GSE34171 OK (FIXED ???) PROBLEMS WITH DOWNLOAD
-# GSE11318 OK (FIXED ???)
-
-#
-# Warning messages:
-#   1: In readLines(fname) :
-#   incomplete final line found on 'C:/Users/Anders/Documents/GitHub/DLBCLdata/GSE19246/GSE19246_series_matrix.txt.gz'
-# 2: In cleanMetadata.GSE10846(meta_data) : NAs introduced by coercion
-# 3: In cleanMetadata.GSE10846(meta_data) : NAs introduced by coercion
-# 4: In cleanMetadata.GSE10846(meta_data) : NAs introduced by coercion
-# 5: In cleanMetadata.GSE10846(meta_data) : NAs introduced by coercion
-# 6: In downloadAndProcessGEO(geo_nbr = gse, cdf = "brainarray", target = "ensg",  :
-#                               Not all downloaded CEL files are in the metadata
-#                             7: In downloadAndProcessGEO(geo_nbr = gse, cdf = "brainarray", target = "ensg",  :
-#                                                           Not all downloaded CEL files are in the metadata
+# GSE19246 downloaded and preprocessed successfully!
+# GSE12195 downloaded and preprocessed successfully!
+# GSE22895 downloaded and preprocessed successfully!
+# GSE31312 downloaded and preprocessed successfully!
+# GSE10846 downloaded and preprocessed successfully!
+# GSE34171 downloaded and preprocessed successfully!
+# GSE22470 downloaded and preprocessed successfully!
+# GSE4475  downloaded and preprocessed successfully!
+# GSE11318 downloaded and preprocessed successfully!
