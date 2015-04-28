8b12c8f7b0d593ee9b7ca4526cdc9d43daedc63c
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2015-04-28 22:08:35 +0200

    Added reference and author.

--- R/readDACData.R
+++ R/readDACData.R
@@ -1,12 +1,21 @@
 #' Read DLBCL class files
 #'
 #' Functions to read the internal data files of classified CEL files using
-#' the DLBCL Automatic Classifier (DAC). These were manually supplied to the
+#' the DLBCL Automatic Classifier (DAC) [1]. These were manually supplied to the
 #' DAC Windows interface.
 #'
 #' @param geo_nbr A \code{character} string giving the GSE number.
 #' @return A \code{data.frame} of the predicted classes and the class
 #'   probabilities.
+#' @author 
+#'   Anders Ellern Bilgrau <abilgrau (at) math.aau.dk> \cr
+#'   Steffen Falgreen
+#' @references
+#'   [1] Care, M. A., Barrans, S., Worrillow, L., Jack, A., Westhead, D. R., &
+#'       Tooze, R. M. (2013). A microarray platform-independent classification 
+#'       tool for cell of origin class allows comparative analysis of gene 
+#'       expression in diffuse large B-cell lymphoma. 
+#'       PloS One, 8(2), e55895. doi:10.1371/journal.pone.0055895
 #' @examples
 #' head(readDACData("GSE19246"))
 #' @export
