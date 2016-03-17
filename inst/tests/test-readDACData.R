df6d5e4b8c084b6ba9ce4cb0e161be6fd5927cd4
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2016-03-17 16:33:40 +0100

    Fixed readDACData test.

--- inst/tests/test-readDACData.R
+++ inst/tests/test-readDACData.R
@@ -1,8 +1,16 @@
 context("Read all DLBCL classifications")
 data(DLBCL_overview)
 
-test_that("readDACData returns data.frame", {
-  for (gse in DLBCL_overview$GSE[-9]) {
-    expect_that(is.data.frame(readDACData(gse)), is_true())
+test_that("readDACData returns a data.frame", {
+  for (gse in DLBCL_overview$GSE) {
+    if (gse == "GSE11318") {
+      next
+    }
+    expect_true(is.data.frame(readDACData(gse)))
   }
+})
+
+test_that("readDACData returns null GSE11318 and other names ", {
+  expect_null(readDACData("GSE11318"))
+  expect_null(readDACData("someothername"))
 })
